4c53b5426a4d81ced3d8fe1c819bf8e7a4b949f8
unknown <hpalenca13@gmail.com>
2024-02-02 15:28:01 -0300

    feat: getCookie

---
+++ scripts/popup.js
@@ -0,0 +1,19 @@
+addEventListener('click', function() {
+    let cookieName = document.getElementById('cookieNameInput').value;
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      // Obtém os cookies da guia ativa
+      chrome.cookies.getAll({ url: tabs[0].url }, function(cookies) {
+        // fazer aparecer no HTML
+        var cookieList = document.createElement('ul');
+        cookies.forEach(function(cookie) {
+        if(cookieName !== cookie.name) return
+          var listItem = document.createElement('li');
+          listItem.textContent = cookie.name + ': ' + cookie.value;
+          cookieList.appendChild(listItem);
+        });
+        
+        document.body.appendChild(cookieList);
+      });
+    });
+  });
