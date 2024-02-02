eb5eafc485c0fc54973808c5d2c044ca8bf40491
Hélio Palença Neto <hpalenca13@gmail.com>
2024-02-02 15:51:38 -0300

    chore: ajuste getCookie

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,19 +1,21 @@
-addEventListener('click', function() {
-    let cookieName = document.getElementById('cookieNameInput').value;
 
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      // Obtém os cookies da guia ativa
-      chrome.cookies.getAll({ url: tabs[0].url }, function(cookies) {
-        // fazer aparecer no HTML
-        var cookieList = document.createElement('ul');
-        cookies.forEach(function(cookie) {
-        if(cookieName !== cookie.name) return
-          var listItem = document.createElement('li');
-          listItem.textContent = cookie.name + ': ' + cookie.value;
-          cookieList.appendChild(listItem);
-        });
-        
-        document.body.appendChild(cookieList);
+var button = document.getElementById("getCookiesButton");
+button.addEventListener('click', function() {
+  let cookieName = document.getElementById('cookieNameInput').value;
+
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // Obtém os cookies da guia ativa
+    chrome.cookies.getAll({ url: tabs[0].url }, function(cookies) {
+      // fazer aparecer no HTML
+      var cookieList = document.createElement('ul');
+      cookies.forEach(function(cookie) {
+      if(cookieName !== cookie.name) return
+        var listItem = document.createElement('li');
+        listItem.textContent = cookie.name + ': ' + cookie.value;
+        cookieList.appendChild(listItem);
       });
+      
+      document.body.appendChild(cookieList);
     });
   });
+});
